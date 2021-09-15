ab7b7302e19619a5db6cb323a8770ecec22dfbd1
Eddy <eddy.nguyen@99designs.com>
2021-09-16 02:39:36 +1000

    Lab 14 - Update update-scope-schema generator to read and update scopes

--- tools/generators/update-scope-schema/index.ts
+++ tools/generators/update-scope-schema/index.ts
@@ -1,8 +1,28 @@
-import { Tree, formatFiles, updateJson } from '@nrwl/devkit';
+import { Tree, formatFiles, updateJson, readJson } from '@nrwl/devkit';
 
-export default async function (tree: Tree, schema: any) {
-  updateJson(tree, 'workspace.json', (value) => {
-    value.defaultProject = schema.defaultProject;
+function getScopes(nxJson: any) {
+  const projects: any[] = Object.values(nxJson.projects);
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags
+        // take only those that point to scope
+        .filter((tag: string) => tag.startsWith('scope:'))
+    )
+    // flatten the array
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    // remove prefix `scope:`
+    .map((scope: string) => scope.slice(6));
+  // remove duplicates
+  return [...new Set(allScopes)];
+}
+
+export default async function (tree: Tree) {
+  const scopes = getScopes(readJson(tree, 'nx.json'));
+  updateJson(tree, 'tools/generators/util-lib/schema.json', (value) => {
+    value.properties.directory['x-prompt'].items = scopes.map((scope) => ({
+      value: scope,
+      label: scope,
+    }));
     return value;
   });
   await formatFiles(tree);
